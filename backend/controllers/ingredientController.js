afcc8533f1f67a7ed452892b3f8a297449a61e11
donniedark0 <darko.milicevicc@elfak.rs>
2022-12-08 21:11:59 +0100

    REST api for ingredients

--- backend/controllers/ingredientController.js
+++ backend/controllers/ingredientController.js
@@ -1,10 +1,14 @@
 const asyncHandler = require('express-async-handler')
 
+const Ingredient = require('../models/ingredientModel')
+
 //@desc     Get ingredients
 //@route    GET /api/goals
 //@access   Private
 const getIngredients =  asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get Ingredients'})
+    const ingredients = await Ingredient.find()
+
+    res.status(200).json(ingredients)
 })
 
 //@desc     Add ingredient
@@ -15,21 +19,45 @@ const addIngredient = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please add a text field')
     }
-    res.status(200).json({message: 'Add Ingredient'})
+
+    const ingredient = await Ingredient.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(ingredient)
 })
 
 //@desc     Update ingredients
 //@route    PUT /api/goals/:id
 //@access   Private
 const updateIngredient = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update Ingredient ${req.params.id}`})
+    const ingredient = await Ingredient.findById(req.params.id)
+
+    if(!ingredient){
+        res.status(400)
+        throw new Error('Ingredient not found')
+    }
+    
+    //new: ture - kreira ga ako ne postoji
+    const updatedIngredient = await Ingredient.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(updatedIngredient)
 })
 
 //@desc     Delete ingredient
 //@route    DELETE /api/goals
 //@access   Private
 const deleteIngredient = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete Ingredient ${req.params.id}`})
+    const ingredient = await Ingredient.findById(req.params.id)
+
+    if(!ingredient){
+        res.status(400)
+        throw new Error('Ingredient not found')
+    }
+
+    await ingredient.remove()
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
